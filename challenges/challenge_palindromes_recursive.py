9511085805f867238ef152b700e76263f38f51e8
eduzissimo <eduardo.alves14@hotmail.com>
2024-06-13 19:04:27 -0300

    Feat: palindromes_recursive feito

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,3 +1,10 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """Faça o código aqui."""
+    if word is None or not isinstance(word, str) or word == "":
+        return False
+    if low_index >= high_index:
+        return True
+    if word[low_index].lower() != word[high_index].lower():
+        return False
+    return is_palindrome_recursive(word, low_index + 1, high_index - 1)
+
     raise NotImplementedError
