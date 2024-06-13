1bec56dfef1809fc04f32244a0f910aacee3e29e
eduzissimo <eduardo.alves14@hotmail.com>
2024-06-13 18:12:06 -0300

    Initial commit: study_schedule feito

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,12 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
+    count = 0
+    if target_time is None:
+        return None
+    for enter_time, exit_time in permanence_period:
+        if not isinstance(enter_time, int) or not isinstance(exit_time, int):
+            return None
+        if target_time >= enter_time and target_time <= exit_time:
+            count += 1      
+    return count
+
     raise NotImplementedError
