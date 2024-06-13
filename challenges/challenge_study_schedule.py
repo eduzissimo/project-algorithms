1e2841db1b6c8737405c6c82ac6f529e4ce99c7e
eduzissimo <eduardo.alves14@hotmail.com>
2024-06-13 18:43:44 -0300

    Feat: test_encrypt

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -6,7 +6,7 @@ def study_schedule(permanence_period, target_time):
         if not isinstance(enter_time, int) or not isinstance(exit_time, int):
             return None
         if target_time >= enter_time and target_time <= exit_time:
-            count += 1      
+            count += 1
     return count
 
     raise NotImplementedError
--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,12 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+from challenges.challenge_encrypt_message import encrypt_message
+import pytest
 
 
 def test_encrypt_message():
-    pass
+    with pytest.raises(TypeError):
+        encrypt_message(123, "xablau")
+        encrypt_message("xablau", 123)
+
+    assert encrypt_message("message", 10) == "egassem"
+    assert encrypt_message("message", 3) == "sem_egas"
+    assert encrypt_message("message", 2) == "egass_em"
